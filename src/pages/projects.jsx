85fdbb762a9b3e0ed41bba79aa61090a8ed9f945
Kimberly Summers <kasummers16@gmail.com>
2024-07-15 20:54:22 -0500

    added more hookd details

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,20 +1,31 @@
+import { Link } from "react-router-dom"
 import "./project.css"
 
 export const Projects = () => {
   return (
     <>
-      <h2>Projects</h2>
+      <h2 className="title">Projects</h2>
+      <h3 className="project-title">Hook'd</h3>
       <div className="project-info">
-        <h3>Hook'd</h3>
         <div className="project-photo">
           <img
-            src="https://res.cloudinary.com/yarn-stash/image/upload/v1721062110/D7918796-BE59-405B-9F02-64C81A6D6433_rwvhys.jpg"
+            src="https://res.cloudinary.com/yarn-stash/image/upload/v1721093488/D7918796-BE59-405B-9F02-64C81A6D6433_1_201_a_imgtdj.jpg"
             alt="homescreen"
-            style={{ width: "55%", height: "250px" }}
+            style={{ width: "45%", height: "250px" }}
           />
           <img
             src="https://res.cloudinary.com/yarn-stash/image/upload/v1721062395/82CD31C0-7253-479B-9072-08C165493059_bwk8p0.png"
             alt="rtsproducts"
+            style={{ width: "45%", height: "250px" }}
+          />
+          <img
+            src="https://res.cloudinary.com/yarn-stash/image/upload/v1721094140/10BC9610-4658-40C7-A1DF-C793F09FE78C_humsbv.jpg"
+            alt="rtsdetail"
+            style={{ width: "45%", height: "250px" }}
+          />
+          <img
+            src="https://res.cloudinary.com/yarn-stash/image/upload/v1721094134/ADDE233C-954B-43FD-9529-4475635E5B01_1_201_a_xrtvsb.jpg"
+            alt="cartview"
             style={{ width: "45%", height: "250px" }}
           />
         </div>
@@ -26,7 +37,19 @@ export const Projects = () => {
             CSS, this platform is designed for my small crochet business. It
             allows customers to purchase pre-made products ready for immediate
             shipping and provides an option for custom product requests, where
-            customers can specify details such as colors and eye styles.
+            customers can specify details such as colors and eye styles. I have
+            also deployed this site using DigitalOcean, though it is not in use
+            by my customers yet.
+          </p>
+          <p>
+            GitHub Repo:{" "}
+            <Link to="https://github.com/KSummers16/hookd-server">Server </Link>
+            <Link to="https://github.com/KSummers16/hookd-client">Client</Link>
+          </p>
+          <p>
+            <Link to="https://monkfish-app-pi2ii.ondigitalocean.app/">
+              Live Website
+            </Link>
           </p>
         </div>
       </div>
